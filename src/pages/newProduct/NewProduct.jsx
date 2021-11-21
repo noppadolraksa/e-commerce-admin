b71547cf1ad18d12021cf67e969792edeafac221
noppadolraksa <90181120+noppadolraksa@users.noreply.github.com>
2021-11-22 01:57:56 +0700

    delete react form

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -12,12 +12,13 @@ import {
   FormControlLabel,
   Checkbox,
   FormHelperText,
+  Switch,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 
 import { DataGrid } from "@mui/x-data-grid";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Box } from "@mui/system";
 
@@ -28,27 +29,53 @@ const textStyle = {
 
 const Container = styled.div`
   flex: 4;
+  margin-bottom: 30px;
+  margin-right: 20px;
 `;
 const Form = styled.div`
   display: flex;
   flex-direction: column;
 `;
 const TextTitle = styled.h2``;
-const ErrorText = styled.p``;
+
 const Text = styled.p``;
+const Textarea = styled.textarea``;
+const Section = styled.section`
+  margin-bottom: 10px;
+`;
+
+const FilterSection = styled.section`
+  border: 1px solid lightblue;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  display: flex;
+  margin-bottom: 10px;
+  justify-content: center;
+  padding: 10px;
+`;
+
+const FilterSectionFlex = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
 const InputImg = styled.input``;
 
 const NewProduct = () => {
-  const {
-    register,
-    handleSubmit,
-
-    formState: { errors },
-  } = useForm();
-
-  const [categories, setCategories] = useState("");
+  // const [categories, setCategories] = useState("");
   const [promotion, setPromotion] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [categories, setCategrories] = useState("");
+  const [img, setImg] = useState("");
+  const [ceilPrice, setCeilPrice] = useState("");
+  const [floorPrice, setFloorPrice] = useState("");
+  const [brand, setBrand] = useState("");
+  const [condition, setCondition] = useState("");
+  const [filterTitleOne, setFilterTitleOne] = useState("");
+  const [filterTitleTwo, setFilterTitleTwo] = useState("");
+  const [filterTitleTwoHappen, setFilterTitleTwoHappen] = useState(false);
 
   const handlePromotion = (event) => {
     if (event.target.checked === true) {
@@ -59,17 +86,17 @@ const NewProduct = () => {
     }
   };
 
-  const handleCategories = (event) => {
-    setCategories(event.target.value);
-  };
-  const onSubmit = async (data, obj, obj2) => {
-    console.log(data, obj, obj2);
+  // const handleCategories = (e) => {
+  //   setCategories(e.target.value);
+  // };
+  const handleSubmit = () => {
+    console.log("submit");
     // try {
     //   const res = await axios.post("http://localhost:8080/auth/register", {
     //     username: data.username,
     //     password: data.password,
     //     email: data.email,
-    //     firstname: data.firstname,
+    //     title: data.title,
     //     lastname: data.lastname,
     //   });
     //   window.location = "/register/success";
@@ -88,157 +115,241 @@ const NewProduct = () => {
     {
       field: "filterTitleOne",
       headerName: "filter1",
-      width: 130,
+      width: 150,
     },
-    { field: "filterTitleTwo", headerName: "filter2", width: 130 },
-    { field: "sku", headerName: "filter2", width: 130 },
+    {
+      field: "filterTitleTwo",
+      headerName: "filter2",
+      width: 150,
+    },
+    { field: "sku", headerName: "sku", width: 130, editable: true },
+    { field: "price", headerName: "price($)", width: 100, editable: true },
+    { field: "stock", headerName: "stock", width: 100, editable: true },
   ];
-  const rows = [{ id: 1, filterTitleOne: "", filterTitleTwo: "", sku: "" }];
+  // useEffect(() => {
+  //   setRows([{ id: 1, filterTitleOne: "", filterTitleTwo: "", sku: "" }]);
+  // }, []);
 
   return (
     <Container>
       <TextTitle>New Product</TextTitle>
-      <Form
-        onSubmit={handleSubmit((data) => onSubmit(data, categories, promotion))}
-      >
-        <TextField
-          sx={textStyle}
-          label="product name*"
-          {...register("title", {
-            required: "you must specify a product name",
-
-            maxLength: {
-              value: 50,
-              message: "product name has 50 characters limit",
-            },
-            pattern: {
-              value: /[A-Za-z0-9]+/i,
-              message: "product name only contain letters and numbers",
-            },
-          })}
-        />
-        {errors.title && <ErrorText>{errors.title.message}</ErrorText>}
-
-        <fieldset style={{ width: 200, color: "blue", marginBottom: "5px" }}>
-          <legend>
-            <Text>description</Text>
-          </legend>
-          <TextareaAutosize
-            style={{ width: 500, border: "none" }}
-            {...register("desc", {
-              required: "you must specify a desc",
-              minLength: {
-                value: 10,
-                message: "desc is too short",
-              },
-              maxLength: {
-                value: 500,
-                message: "desc has 500 characters limit",
-              },
-            })}
-            placeholder="it can contain up to 500 characters.."
-            aria-label="minimum height"
-            minRows={10}
-          />
-          {errors.desc && <ErrorText>{errors.desc.message}</ErrorText>}
-        </fieldset>
-
-        <InputImg
-          style={{ marginBottom: 5 }}
-          type="file"
-          name="img"
-          accept="image/*"
-        />
-
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Categories*</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
+      <Form>
+        <Section>
+          <TextField
+            size="small"
+            id="title"
+            variant="outlined"
+            label="Title"
             sx={textStyle}
-            value={categories}
-            onChange={handleCategories}
-            {...register("categories", {
-              required: "you must specify categories",
-            })}
-          >
-            <MenuItem value="Shirt">Shirt</MenuItem>
-            <MenuItem value="Plain">Plain</MenuItem>
-            <MenuItem value="Women">Women</MenuItem>
-          </Select>
-        </FormControl>
-        {errors.categories && (
-          <ErrorText>{errors.categories.message}</ErrorText>
-        )}
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Section>
 
-        <Box sx={{ display: "flex" }}>
-          <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
-            <FormLabel component="legend">Promotion</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    size="small"
-                    onChange={handlePromotion}
-                    name="ร้านค้าแนะนำ"
-                  />
-                }
-                label="ร้านค้าแนะนำ"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    size="small"
-                    onChange={handlePromotion}
-                    name="exclusive price"
-                  />
-                }
-                label="exclusive price"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    size="small"
-                    onChange={handlePromotion}
-                    name="only9.9$"
-                  />
-                }
-                label="only9.9$"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    size="small"
-                    onChange={handlePromotion}
-                    name="10%cashback"
-                  />
-                }
-                label="10%cashback"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    size="small"
-                    onChange={handlePromotion}
-                    name="9.9$free shipping"
-                  />
-                }
-                label="9.9$free shipping"
-              />
-            </FormGroup>
-            <FormHelperText>these can be selected more than one</FormHelperText>
+        <Section>
+          <TextField
+            size="small"
+            id="brand"
+            variant="outlined"
+            label="Brand"
+            sx={textStyle}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+        </Section>
+
+        <Section>
+          <fieldset style={{ width: 200, color: "blue" }}>
+            <legend>
+              <Text>description*</Text>
+            </legend>
+            <Textarea
+              style={{ width: 500, border: "none", minHeight: "100px" }}
+              placeholder="it can contain up to 500 characters.."
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </fieldset>
+        </Section>
+
+        <Section>
+          <InputImg
+            type="file"
+            name="img"
+            accept="image/*"
+            onChange={(e) => setImg(e.target.value)}
+          />
+        </Section>
+
+        <Section>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Categories*</InputLabel>
+            <Select
+              size="small"
+              labelId="demo-simple-select-label"
+              sx={textStyle}
+              value={categories}
+              label="Categories*"
+              onChange={(e) => setCategrories(e.target.value)}
+            >
+              <MenuItem value="Shirt">Shirt</MenuItem>
+              <MenuItem value="Plain">Plain</MenuItem>
+              <MenuItem value="Women">Women</MenuItem>
+            </Select>
           </FormControl>
-        </Box>
+        </Section>
 
-        <div style={{ height: 400, width: "100%" }}>
+        <Section>
+          <FormControl fullWidth>
+            <InputLabel id="contition-simple-select-label">
+              Condition*
+            </InputLabel>
+            <Select
+              size="small"
+              labelId="condition-simple-select-label"
+              sx={textStyle}
+              value={condition}
+              label="Condition*"
+              onChange={(e) => setCondition(e.target.value)}
+            >
+              <MenuItem value="New">New</MenuItem>
+              <MenuItem value="Used">Used</MenuItem>
+            </Select>
+          </FormControl>
+        </Section>
+
+        <Section>
+          <Box sx={{ display: "flex" }}>
+            <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
+              <FormLabel component="legend">Promotion</FormLabel>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      size="small"
+                      onChange={handlePromotion}
+                      name="ร้านค้าแนะนำ"
+                    />
+                  }
+                  label="ร้านค้าแนะนำ"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      size="small"
+                      onChange={handlePromotion}
+                      name="exclusive price"
+                    />
+                  }
+                  label="exclusive price"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      size="small"
+                      onChange={handlePromotion}
+                      name="only9.9$"
+                    />
+                  }
+                  label="only9.9$"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      size="small"
+                      onChange={handlePromotion}
+                      name="10%cashback"
+                    />
+                  }
+                  label="10%cashback"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      size="small"
+                      onChange={handlePromotion}
+                      name="9.9$free shipping"
+                    />
+                  }
+                  label="9.9$free shipping"
+                />
+              </FormGroup>
+              <FormHelperText>
+                these can be selected more than one
+              </FormHelperText>
+            </FormControl>
+          </Box>
+        </Section>
+        <hr />
+        <br />
+        <FormControlLabel
+          sx={{
+            display: "block",
+          }}
+          control={
+            <Switch
+              size="small"
+              checked={filterTitleTwoHappen}
+              onChange={() => setFilterTitleTwoHappen(!filterTitleTwoHappen)}
+              name="Filter Product two ?"
+              color="primary"
+            />
+          }
+          label="Filter Product two ?"
+        />
+        <FilterSection>
+          <FilterSectionFlex style={{ width: "45%" }}>
+            <TextField
+              style={{ marginRight: "10px" }}
+              variant="outlined"
+              label="Brand"
+              size="small"
+              sx={textStyle}
+              onChange={(e) => setBrand(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              size="small"
+              sx={textStyle}
+              style={{ marginRight: "10px" }}
+            >
+              add filter one({`${1}/5`})
+            </Button>
+          </FilterSectionFlex>
+          {filterTitleTwoHappen && (
+            <FilterSectionFlex style={{ width: "45%" }}>
+              <TextField
+                size="small"
+                variant="outlined"
+                label="Brand"
+                sx={textStyle}
+                onChange={(e) => setBrand(e.target.value)}
+              />
+              <Button
+                variant="outlined"
+                size="small"
+                sx={textStyle}
+                style={{ marginRight: "10px" }}
+              >
+                add filter two({`${1}/5`})
+              </Button>
+            </FilterSectionFlex>
+          )}
+        </FilterSection>
+
+        <Section style={{ height: 400 }}>
           <DataGrid
             rows={rows}
             columns={columns}
             pageSize={5}
             rowsPerPageOptions={[5]}
-            checkboxSelection
           />
-        </div>
-        <Button variant="contained" type="submit">
+        </Section>
+
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ width: 100 }}
+          onClick={handleSubmit}
+        >
           SUBMIT
         </Button>
       </Form>
---
+++ src/pages/newProduct/NewProduct2.jsx
@@ -0,0 +1,111 @@
+//  <fieldset style={{ width: 200, color: "blue", marginBottom: "5px" }}>
+//         <legend>
+//           <Text>description</Text>
+//         </legend>
+//         <Textarea
+//           style={{ width: 500, border: "none", minHeight: "100px" }}
+//           // {...register("desc", {
+//           //   required: "you must specify a desc",
+//           //   minLength: {
+//           //     value: 10,
+//           //     message: "desc is too short",
+//           //   },
+//           //   maxLength: {
+//           //     value: 500,
+//           //     message: "desc has 500 characters limit",
+//           //   },
+//           // })}
+//           placeholder="it can contain up to 500 characters.."
+//           aria-label="minimum height"
+//         />
+//         {errors.desc && <ErrorText>{errors.desc.message}</ErrorText>}
+//       </fieldset>
+//       <InputImg
+//         style={{ marginBottom: 5 }}
+//         type="file"
+//         name="img"
+//         accept="image/*"
+//       />
+
+//       <Section>
+//         <Controller
+//           render={({ field }) => (
+//             <FormControl fullWidth>
+//               <InputLabel id="demo-simple-select-label">Categories*</InputLabel>
+//               <Select
+//                 labelId="demo-simple-select-label"
+//                 sx={textStyle}
+//                 {...field}
+//                 // inputRef={register("categories", {
+//                 //   required: "you must specify categories",
+//                 // })}
+//               >
+//                 <MenuItem value="Shirt">Shirt</MenuItem>
+//                 <MenuItem value="Plain">Plain</MenuItem>
+//                 <MenuItem value="Women">Women</MenuItem>
+//               </Select>
+//             </FormControl>
+//           )}
+//           name="categories"
+//           control={control}
+//         />
+//       </Section>
+//       {errors.categories && <ErrorText>{errors.categories.message}</ErrorText>}
+//       <Box sx={{ display: "flex" }}>
+//         <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
+//           <FormLabel component="legend">Promotion</FormLabel>
+//           <FormGroup>
+//             <FormControlLabel
+//               control={
+//                 <Checkbox
+//                   size="small"
+//                   onChange={handlePromotion}
+//                   name="ร้านค้าแนะนำ"
+//                 />
+//               }
+//               label="ร้านค้าแนะนำ"
+//             />
+//             <FormControlLabel
+//               control={
+//                 <Checkbox
+//                   size="small"
+//                   onChange={handlePromotion}
+//                   name="exclusive price"
+//                 />
+//               }
+//               label="exclusive price"
+//             />
+//             <FormControlLabel
+//               control={
+//                 <Checkbox
+//                   size="small"
+//                   onChange={handlePromotion}
+//                   name="only9.9$"
+//                 />
+//               }
+//               label="only9.9$"
+//             />
+//             <FormControlLabel
+//               control={
+//                 <Checkbox
+//                   size="small"
+//                   onChange={handlePromotion}
+//                   name="10%cashback"
+//                 />
+//               }
+//               label="10%cashback"
+//             />
+//             <FormControlLabel
+//               control={
+//                 <Checkbox
+//                   size="small"
+//                   onChange={handlePromotion}
+//                   name="9.9$free shipping"
+//                 />
+//               }
+//               label="9.9$free shipping"
+//             />
+//           </FormGroup>
+//           <FormHelperText>these can be selected more than one</FormHelperText>
+//         </FormControl>
+//       </Box>
